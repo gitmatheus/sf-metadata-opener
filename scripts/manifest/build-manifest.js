361842aeaf42045de39a8357d911b4dee62a6498
Matheus Goncalves <matheus.goncalves@salesforce.com>
2025-07-28 14:55:05 -0700

    Moving package config to separate files and creating scripts

---
+++ scripts/manifest/build-manifest.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+
+const basePackage = JSON.parse(fs.readFileSync("package.base.json", "utf8"));
+const commands = require("./commands.json");
+const menus = require("./menus.json");
+const activationEvents = require("./activationEvents.json");
+
+basePackage.contributes.commands = commands;
+basePackage.contributes.menus = menus;
+basePackage.activationEvents = activationEvents;
+
+const outputPath = path.resolve(__dirname, "../../package.json");
+
+fs.writeFileSync(outputPath, JSON.stringify(basePackage, null, 2));
+console.log("✅ package.json was created or overwritten successfully.");
