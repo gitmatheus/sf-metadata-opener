eddfb56d9367c19cfb01456e9cfa2224697f6b34
Matheus Goncalves <matheus.goncalves@salesforce.com>
2025-08-01 19:09:57 -0700

    Adding menu and Command Palette entries

--- src/openers/index.ts
+++ src/openers/index.ts
@@ -1,5 +1,6 @@
 export * as bot from "./bot/helpers";
 export * as dashboard from "./dashboard/helpers";
+export * as flexipage from "./flexipage/helpers";
 export * as flow from "./flow/helpers";
 export * as report from "./report/helpers";
 export * as sobject from "./sobject/helpers";
